44f6da98df26f3cde49a2639114c204cbb3d315d
Gianmarco Caputo <gianmarco.caputo.uk@gmail.com>
2021-08-07 00:26:00 +0100

    Implement FileCreator class.

---
+++ src/main/java/io/github/vonathar/FileCreator.java
@@ -0,0 +1,31 @@
+package io.github.vonathar;
+
+import io.github.vonathar.exception.FileCreationException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.FileAlreadyExistsException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class FileCreator {
+
+  private final Path downloadPath;
+  private final AtomicInteger downloadCount = new AtomicInteger(0);
+
+  public FileCreator(Path downloadPath) {
+    this.downloadPath = downloadPath;
+  }
+
+  public synchronized void create(InputStream stream) throws FileCreationException {
+    String name = String.valueOf(downloadCount.getAndIncrement());
+    Path path = downloadPath.resolve(name);
+    try {
+      Files.copy(stream, path);
+    } catch (FileAlreadyExistsException e) {
+      create(stream);
+    } catch (IOException e) {
+      throw new FileCreationException(path, e);
+    }
+  }
+}
---
+++ src/main/java/io/github/vonathar/exception/FileCreationException.java
@@ -0,0 +1,10 @@
+package io.github.vonathar.exception;
+
+import java.nio.file.Path;
+
+public class FileCreationException extends Exception {
+
+  public FileCreationException(Path path, Throwable cause) {
+    super(String.format("Failed to create file: %s", path), cause);
+  }
+}
