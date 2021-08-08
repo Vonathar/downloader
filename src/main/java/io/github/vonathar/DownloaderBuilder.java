d38be7d8a59ec4b00fb7c30fc8dccfbcd94775be
Gianmarco Caputo <gianmarco.caputo.uk@gmail.com>
2021-08-08 15:40:27 +0100

    Add buildUrls/buildOutputDirectory methods to DownloaderBuilder class.

--- src/main/java/io/github/vonathar/DownloaderBuilder.java
+++ src/main/java/io/github/vonathar/DownloaderBuilder.java
@@ -1,6 +1,8 @@
 package io.github.vonathar;
 
 import io.github.vonathar.exception.NoUrlsFoundException;
+import io.github.vonathar.io.DirectoryCreator;
+import io.github.vonathar.io.DirectoryStrategy;
 import io.github.vonathar.io.UrlFileParser;
 import java.net.URI;
 import java.nio.file.Path;
@@ -13,68 +15,74 @@ public class DownloaderBuilder {
 
   private final Logger log = LogManager.getLogger(DownloaderBuilder.class);
 
-  private Set<URI> urls;
+  private final Set<URI> urls = new HashSet<>();
   private Path urlFile;
   private Path outputDirectory;
   private int numThreads;
   private int minSleep;
   private int maxSleep;
   private boolean logProgress;
+  private DirectoryStrategy directoryStrategy;
 
-  public DownloaderBuilder setUrls(Set<URI> urls) {
-    this.urls = urls;
+  public DownloaderBuilder addUrls(Set<URI> urls) {
+    this.urls.addAll(urls);
     return this;
   }
 
-  public DownloaderBuilder setUrlFile(Path urlFile) {
+  public DownloaderBuilder urlFile(Path urlFile) {
     this.urlFile = urlFile;
     return this;
   }
 
-  public DownloaderBuilder setOutputDirectory(Path outputDirectory) {
+  public DownloaderBuilder outputDirectory(Path outputDirectory) {
     this.outputDirectory = outputDirectory;
     return this;
   }
 
-  public DownloaderBuilder setNumThreads(int numThreads) {
+  public DownloaderBuilder numThreads(int numThreads) {
     this.numThreads = numThreads;
     return this;
   }
 
-  public DownloaderBuilder setMinSleep(int minSleep) {
+  public DownloaderBuilder minSleep(int minSleep) {
     this.minSleep = minSleep;
     return this;
   }
 
-  public DownloaderBuilder setMaxSleep(int maxSleep) {
+  public DownloaderBuilder maxSleep(int maxSleep) {
     this.maxSleep = maxSleep;
     return this;
   }
 
-  public DownloaderBuilder setLogProgress(boolean logProgress) {
+  public DownloaderBuilder logProgress(boolean logProgress) {
     this.logProgress = logProgress;
     return this;
   }
 
+  public DownloaderBuilder directoryStrategy(DirectoryStrategy directoryStrategy) {
+    this.directoryStrategy = directoryStrategy;
+    return this;
+  }
+
   public Downloader build() {
-    Set<URI> urls = buildUrls();
+    buildUrls();
+    buildOutputDirectory();
     return new Downloader(urls, outputDirectory, numThreads, minSleep, maxSleep, logProgress);
   }
 
-  private Set<URI> buildUrls() {
-    Set<URI> urls = new HashSet<>();
-    if (this.urls != null) {
-      urls.addAll(this.urls);
-    }
-    if (this.urlFile != null) {
+  private void buildUrls() {
+    if (urlFile != null) {
       UrlFileParser urlFileParser = new UrlFileParser();
-      urls.addAll(urlFileParser.parse(this.urlFile));
+      urls.addAll(urlFileParser.parse(urlFile));
     }
     if (urls.isEmpty()) {
       throw new NoUrlsFoundException("Failed to find any URL to download.");
     }
-
     log.info("{} urls found.", urls.size());
-    return urls;
+  }
+
+  private void buildOutputDirectory() {
+    DirectoryCreator directoryCreator = new DirectoryCreator(directoryStrategy);
+    directoryCreator.create(outputDirectory);
   }
 }
