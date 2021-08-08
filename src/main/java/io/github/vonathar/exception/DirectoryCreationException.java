c4d274ca818d7050f5bd99f0c8346bf44b0a30d2
Gianmarco Caputo <gianmarco.caputo.uk@gmail.com>
2021-08-08 14:47:02 +0100

    Implement DirectoryCreationException and DirectoryStrategy classes.

---
+++ src/main/java/io/github/vonathar/exception/DirectoryCreationException.java
@@ -0,0 +1,8 @@
+package io.github.vonathar.exception;
+
+public class DirectoryCreationException extends RuntimeException {
+
+  public DirectoryCreationException(String message) {
+    super(message);
+  }
+}
---
+++ src/main/java/io/github/vonathar/io/DirectoryStrategy.java
@@ -0,0 +1,7 @@
+package io.github.vonathar.io;
+
+public enum DirectoryStrategy {
+  DELETE_EXISTING,
+  RENAME_CREATED,
+  REUSE_EXISTING
+}
