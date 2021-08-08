6d206bbf1160c3a47dae9ae7707ef091dbc17c76
Gianmarco Caputo <gianmarco.caputo.uk@gmail.com>
2021-08-08 18:08:23 +0100

    Fix parsing of user_agents file.

--- src/main/java/io/github/vonathar/exception/FileParsingException.java
+++ src/main/java/io/github/vonathar/exception/FileParsingException.java
@@ -7,4 +7,12 @@ public class FileParsingException extends RuntimeException {
   public FileParsingException(Path path, Throwable cause) {
     super("Failed to parse file: " + path, cause);
   }
+
+  public FileParsingException(String message) {
+    super(message);
+  }
+
+  public FileParsingException(String message, Throwable cause) {
+    super(message, cause);
+  }
 }
--- src/main/java/io/github/vonathar/http/UserAgentGenerator.java
+++ src/main/java/io/github/vonathar/http/UserAgentGenerator.java
@@ -1,15 +1,11 @@
 package io.github.vonathar.http;
 
-import io.github.vonathar.exception.FileParsingException;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
+import io.github.vonathar.io.UserAgentFileParser;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.concurrent.ThreadLocalRandom;
-import java.util.stream.Stream;
 
 public class UserAgentGenerator {
 
@@ -17,16 +13,8 @@ public class UserAgentGenerator {
   private final Set<Integer> usedIndexes = new HashSet<>();
 
   public UserAgentGenerator() {
-    loadList();
-  }
-
-  private void loadList() {
-    Path file = Path.of("src/main/resources/user_agents");
-    try (Stream<String> stream = Files.lines(file)) {
-      stream.forEach(values::add);
-    } catch (IOException e) {
-      throw new FileParsingException(file, e);
-    }
+    UserAgentFileParser userAgentFileParser = new UserAgentFileParser();
+    values.addAll(userAgentFileParser.parse());
   }
 
   public String random() {
